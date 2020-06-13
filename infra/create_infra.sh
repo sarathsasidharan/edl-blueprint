0d70e0937a4e92096fb565bd9096be32c3f384a5
Sarath <sasasid@microsoft.com>
2020-06-13 10:24:39 +0200

    refractor

--- infra/create_infra.sh
+++ infra/create_infra.sh
@@ -2,28 +2,17 @@
 # Deploy an Enterprise Data Lake Infra on Azure , this is the master script
 
 #Declare Script Paths
+CONFIG_PATH="config/azure.config"
+LOGIN_TEARDOWN_PATH="login/login.sh"
 NETWORK_SCRIPT="network/create-network.sh"
 RESOURCE_GROUP_SCRIPT="resource-group/create-rg.sh"
-CONFIG_PATH="config/azure.config"
+
 
 #Import Functions needed to create Infra.
 source $CONFIG_PATH
+source $LOGIN_TEARDOWN_PATH
 source $NETWORK_CREATION_SCRIPT
 source $RESOURCE_GROUP_SCRIPT
-
-
-
-#Function to Login /Delete / Can be removed when triggered from AzDevOps
-login() {
-    echo "Login to Azure"
-    az login
-}
-
-shutdown() {
-    MARKED_RESOURCE_GROUP=$1
-    echo "Shutting down ETL Infra , decomissioning resources"
-    az group delete -n $MARKED_RESOURCE_GROUP
-}
 
 
 #################### Start Creation ########################
---
+++ infra/network/create-peering.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+# Deploy an Enterprise Data Lake Infra on Azure
+
+CONFIG_PATH="config/azure.config"
+
+#Import Functions needed to create Infra.
+source $CONFIG_PATH
+
+echo "This is where networking starts"
+
+create_virtual_network_peering()
+{
+  DEPARTMENT_NAME=$1
+  VNET_RANGE=$2
+
+  echo "VNET FOR $DEPARTMENT_NAME"
+  az network vnet create -g $RG_LOCATION -n $DEPARTMENT_NAME+"_VNET" --address-prefix $VNET_RANGE
+}
