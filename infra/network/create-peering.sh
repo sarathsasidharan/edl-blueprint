9f5c4331165ae37874e346da8b725fb82cbd5b23
Sarath <sasasid@microsoft.com>
2020-06-19 10:39:56 +0200

    Completed peering

--- infra/network/create-peering.sh
+++ infra/network/create-peering.sh
@@ -10,14 +10,22 @@ create_virtual_network_peering()
   RESOURCE_GRP_REMOTE=$2
   DEPARTMENT_VNET="$3_vnet"
   DEPARTMENT_REMOTE_VNET="$4_vnet"
+  DEPARTMENT_VNET_NAME=$(az resource show -g $RESOURCE_GRP --resource-type Microsoft.Network/virtualNetworks -n $DEPARTMENT_VNET --query id -o tsv)
   DEPARTMENT_REMOTE_VNET_NAME=$(az resource show -g $RESOURCE_GRP_REMOTE --resource-type Microsoft.Network/virtualNetworks -n $DEPARTMENT_REMOTE_VNET --query id -o tsv)
-  
+
 
   echo "VNET Peering FOR $DEPARTMENT_VNET and $DEPARTMENT_REMOTE_VNET" 
   az network vnet peering create -g $RESOURCE_GRP \
                                  -n $DEPARTMENT_REMOTE_VNET"_peering" \
                                  --vnet-name $DEPARTMENT_VNET \
                                  --remote-vnet $DEPARTMENT_REMOTE_VNET_NAME \
+                                 --allow-vnet-access
+
+  echo "Reverse VNET Peering FOR $DEPARTMENT_REMOTE_VNET and $DEPARTMENT_VNET" 
+  az network vnet peering create -g $RESOURCE_GRP_REMOTE \
+                                 -n $DEPARTMENT_VNET"_peering" \
+                                 --vnet-name $DEPARTMENT_REMOTE_VNET \
+                                 --remote-vnet $DEPARTMENT_VNET_NAME \
                                  --allow-vnet-access
 }
 
