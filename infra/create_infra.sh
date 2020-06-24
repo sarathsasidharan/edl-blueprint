6babe4275f3bb5463a1ea7e5f69f8310e5428281
Sarath <sasasid@microsoft.com>
2020-06-24 16:00:29 +0200

    Added params to assignment

--- infra/create_infra.sh
+++ infra/create_infra.sh
@@ -11,13 +11,19 @@ POLICY_PATH="policy/policies.sh"
 
 
 #Policy Variables
-POLICY_REGION="audit-location-data-lake"
-POLICY_REGION_ID="e56962a6-4747-49cd-b67b-bf8b01975c4c"
-
+POLICY_REGION_DISP="audit-location-data-lake"
+POLICY_REGION="e56962a6-4747-49cd-b67b-bf8b01975c4c"
+POLICY_REGION_PARAM="regions.parameters.json"
 
 POLICY_VNET="vm-creation-in-approved-vnet"
-POLICY_VNET_DISPLAY_NAME="Use approved vNet for VM network interfaces"
-POLICY_VNET_DESC="This policy enforces VM network interfaces to use vNet."
+POLICY_VNET_DISPLAY_NAME="Use approved vNet for Deploying VMs"
+POLICY_VNET_DESC="This policy enforces VMs to be deployed in approved vNets."
+POLICY_VNET_DISP="audit-vm-deployment-data-lake"
+POLICY_VNET0_PARAM="vnet0.parameters.json"
+POLICY_VNET1_PARAM="vnet1.parameters.json"
+POLICY_VNET2_PARAM="vnet2.parameters.json"
+POLICY_VNET3_PARAM="vnet3.parameters.json"
+POLICY_VNET4_PARAM="vnet4.parameters.json"
 
 #Import Functions needed to create Infra.
 source $CONFIG_PATH
@@ -35,36 +41,41 @@ echo "This is where the Base EDL Infrastructure is Spun Up"
 
 azlogin 
 
-create_resource_group $RG_NAME_DEP0 $RG_LOCATION
-create_resource_group $RG_NAME_DEP1 $RG_LOCATION
-create_resource_group $RG_NAME_DEP2 $RG_LOCATION
-create_resource_group $RG_NAME_DEP3 $RG_LOCATION
-create_resource_group $RG_NAME_DEP4 $RG_LOCATION
+#create_resource_group $RG_NAME_DEP0 $RG_LOCATION
+#create_resource_group $RG_NAME_DEP1 $RG_LOCATION
+#create_resource_group $RG_NAME_DEP2 $RG_LOCATION
+#create_resource_group $RG_NAME_DEP3 $RG_LOCATION
+#create_resource_group $RG_NAME_DEP4 $RG_LOCATION
+#
+#create_virtual_network $RG_NAME_DEP0 $DEPARTMENT_0 $VNET_DEP0 
+#create_virtual_network $RG_NAME_DEP1 $DEPARTMENT_1 $VNET_DEP1
+#create_virtual_network $RG_NAME_DEP2 $DEPARTMENT_2 $VNET_DEP2
+#create_virtual_network $RG_NAME_DEP3 $DEPARTMENT_3 $VNET_DEP3
+#create_virtual_network $RG_NAME_DEP4 $DEPARTMENT_4 $VNET_DEP4
+#
+#
+#create_virtual_network_peering $RG_NAME_DEP0 $RG_NAME_DEP1 $DEPARTMENT_0 $DEPARTMENT_1
+#create_virtual_network_peering $RG_NAME_DEP0 $RG_NAME_DEP2 $DEPARTMENT_0 $DEPARTMENT_2
+#create_virtual_network_peering $RG_NAME_DEP0 $RG_NAME_DEP3 $DEPARTMENT_0 $DEPARTMENT_3
+#create_virtual_network_peering $RG_NAME_DEP0 $RG_NAME_DEP4 $DEPARTMENT_0 $DEPARTMENT_4
+#
+##Create Custom Policies
+#create_policy $POLICY_VNET $POLICY_VNET_DISPLAY_NAME $POLICY_VNET_DESC 
+#
+##Assign location restriction policy
+#assign_policy $POLICY_REGION_DISP $RG_NAME_DEP0 $POLICY_REGION $POLICY_REGION_PARAM
+#assign_policy $POLICY_REGION_DISP $RG_NAME_DEP1 $POLICY_REGION $POLICY_REGION_PARAM
+#assign_policy $POLICY_REGION_DISP $RG_NAME_DEP2 $POLICY_REGION $POLICY_REGION_PARAM
+#assign_policy $POLICY_REGION_DISP $RG_NAME_DEP3 $POLICY_REGION $POLICY_REGION_PARAM
+#assign_policy $POLICY_REGION_DISP $RG_NAME_DEP4 $POLICY_REGION $POLICY_REGION_PARAM
 
-create_virtual_network $RG_NAME_DEP0 $DEPARTMENT_0 $VNET_DEP0
-create_virtual_network $RG_NAME_DEP1 $DEPARTMENT_1 $VNET_DEP1
-create_virtual_network $RG_NAME_DEP2 $DEPARTMENT_2 $VNET_DEP2
-create_virtual_network $RG_NAME_DEP3 $DEPARTMENT_3 $VNET_DEP3
-create_virtual_network $RG_NAME_DEP4 $DEPARTMENT_4 $VNET_DEP4
+#Assign vnet restriction on resource groups level
+assign_policy $POLICY_VNET_DISP $RG_NAME_DEP0 $POLICY_VNET $POLICY_VNET0_PARAM
+assign_policy $POLICY_VNET_DISP $RG_NAME_DEP1 $POLICY_VNET $POLICY_VNET1_PARAM
+assign_policy $POLICY_VNET_DISP $RG_NAME_DEP2 $POLICY_VNET $POLICY_VNET2_PARAM
+assign_policy $POLICY_VNET_DISP $RG_NAME_DEP3 $POLICY_VNET $POLICY_VNET3_PARAM
+assign_policy $POLICY_VNET_DISP $RG_NAME_DEP4 $POLICY_VNET $POLICY_VNET4_PARAM
 
-
-create_virtual_network_peering $RG_NAME_DEP0 $RG_NAME_DEP1 $DEPARTMENT_0 $DEPARTMENT_1
-create_virtual_network_peering $RG_NAME_DEP0 $RG_NAME_DEP2 $DEPARTMENT_0 $DEPARTMENT_2
-create_virtual_network_peering $RG_NAME_DEP0 $RG_NAME_DEP3 $DEPARTMENT_0 $DEPARTMENT_3
-create_virtual_network_peering $RG_NAME_DEP0 $RG_NAME_DEP4 $DEPARTMENT_0 $DEPARTMENT_4
-
-
-create_policy $POLICY_VNET $POLICY_VNET_DISPLAY_NAME $POLICY_VNET_DESC 
-
-#assign location restriction policy
-assign_policy $POLICY_REGION $RG_NAME_DEP0 $POLICY_REGION_ID 
-assign_policy $POLICY_REGION $RG_NAME_DEP1 $POLICY_REGION_ID 
-assign_policy $POLICY_REGION $RG_NAME_DEP2 $POLICY_REGION_ID 
-assign_policy $POLICY_REGION $RG_NAME_DEP3 $POLICY_REGION_ID 
-assign_policy $POLICY_REGION $RG_NAME_DEP4 $POLICY_REGION_ID 
-
-#assign vnet restriction on resource groups
-assign_policy $POLICY_VNET $RG_NAME_DEP0 $POLICY_VNET 
 
 
 #shutdown $RG_NAME
--- infra/policy/policies.sh
+++ infra/policy/policies.sh
@@ -6,7 +6,9 @@ assign_policy(){
     POLICY_NAME=$1
     RESOURCE_GRP=$(az group show -n $2 --query id -o tsv)
     POLICY_ID=$3
-    PARAM_PATH="policy/$POLICY_NAME/azurepolicy.parameters.json"
+    PARAM_PATH="policy/$POLICY_NAME/assignment/$4"
+
+    
     
     echo "Creating Policy $POLICY_NAME for $2"
 
@@ -22,8 +24,8 @@ create_policy(){
     POLICY_NAME=$1
     DISPLAY_NAME=$2
     DESCRIPTION=$3
-    RULES_PATH=policy/$POLICY_NAME/azurepolicy.rules.json
-    PARAM_PATH=policy/$POLICY_NAME/azurepolicy.parameters.json
+    RULES_PATH=policy/$POLICY_NAME/definition/$POLICY_NAME.rules.json
+    PARAM_PATH=policy/$POLICY_NAME/definition/$POLICY_NAME.parameters.json
     
     echo "Creating new Custom Policy $POLICY_NAME"
     az policy definition create --name $POLICY_NAME \
